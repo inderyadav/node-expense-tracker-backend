8125acf3501c3b5a410afca205e098465e6caed9
Indrajeet <inder.yadav136@gmail.com>
2026-06-03 12:12:17 +0530

    updated route file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,4 +7,8 @@ const auth = require('../controllers/authController');
 router.post('/register', auth.register);
 router.post('/login', auth.login);
 
+app.get('/', (req, res) => {
+  res.send('Expense Tracker Backend Running 🚀');
+});
+
 module.exports = router;
